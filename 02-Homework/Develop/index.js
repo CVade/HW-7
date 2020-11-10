77c23d53e1507584b2512273d839e50938cabcbd
CVade <christian.vadevoulis@gmail.com>
2020-11-09 20:33:37 -0600

    Finished assignment

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,15 +1,70 @@
+const fs = require("fs")
+
+const path = require("path")
+
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is the title of your Project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Give your project a description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation Instructions",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "License",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Contributing",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Tests",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Email",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "GitHub",
+        name: "github"
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((inquirerResponses)=> {
+        console.log(inquirerResponses);
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+    })
 }
 
 // function call to initialize program
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -2,7 +2,44 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+  \`\`\`
+  ${data.description}
+  \`\`\`
+  ## Table of Contents
+  *[Installation](#Installation)
+  *[Usage](#Usage)
+  *[License](#License)
+  *[Contributing](#Contributing)
+  *[Tests](#Tests)
+  *[Questions](#Questions)
+  ## Installation
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  ## Usage
+  \`\`\`
+  ${data.usage}
+  \`\`\`
+  ## License
+  \`\`\`
+  ${data.license}
+  \`\`\`
+  ## Contributing
+  \`\`\`
+  ${data.contributing}
+  \`\`\`
+  ## Tests
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+  ## Questions
+  Feel Free to ask me questions via Email or GitHub, linked below:
+  
+  Email: ${data.email}
+  Github: [${data.github}](https://github.com/${data.github})
+  `;
 }
+
 
 module.exports = generateMarkdown;
